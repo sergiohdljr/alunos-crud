36563ea7d16a799dcf9e7fe6816c523ec493fb22
sergiohdljr <sergio.tjf1@gmail.com>
2025-11-08 12:06:00 -0300

    feat: add use case for listing students with repository integration

---
+++ api/src/application/use-cases/list-students-use-case.ts
@@ -0,0 +1,12 @@
+import { StudentRepository } from "../repositories/student-repository-interface.ts";
+import { Student } from "@/domain/entities/student.ts";
+
+export class ListStudentsUseCase {
+    constructor(private readonly studentRepository: StudentRepository) {}
+
+    async execute(student: Partial<Student>): Promise<Student[]> {
+        const students = await this.studentRepository.list(student);
+        return students;
+    }
+}
+    
