adc0f700094c5387d65a6114649dfb5a5177f815
sergiohdljr <sergio.tjf1@gmail.com>
2025-11-08 11:49:22 -0300

    feat: implement DrizzleRepository with create and list methods for student data

---
+++ api/src/infra/repositories/drizzle-repository.ts
@@ -0,0 +1,31 @@
+import { StudentRepository } from "@/application/repositories/student-repository-interface.ts";
+import { Student } from "@/domain/entities/student.ts";
+import { studentTable } from "@/infra/db/schema/students.ts";
+import { eq, or } from "drizzle-orm";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
+
+export class DrizzleRepository implements StudentRepository {
+    constructor(private readonly db: NodePgDatabase<Record<string, never>>) {}
+
+    async create(student: Student): Promise<Record<string, number>> {
+        const [newStudent] = await this.db
+                .insert(studentTable)
+                .values(student)
+                .returning({id: studentTable.id})
+
+        return newStudent;
+    }
+    async list(student: Partial<Student>): Promise<Student[]> {
+        const conditions = [];
+        if (student.name) conditions.push(eq(studentTable.name, student.name));
+        if (student.email) conditions.push(eq(studentTable.email, student.email));
+        if (student.cpf) conditions.push(eq(studentTable.cpf, student.cpf));
+        
+        return await this.db
+            .select()
+            .from(studentTable)
+            .where(conditions.length > 0 ? or(...conditions) : undefined);
+    }
+    
+}   
+    
