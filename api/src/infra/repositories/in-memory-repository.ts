84474f4c2095b03ddb9488008cb0558f27f4ba40
sergiohdljr <sergio.tjf1@gmail.com>
2025-11-08 12:17:25 -0300

    feat: implement in-memory student repository with create and list methods

---
+++ api/src/infra/repositories/in-memory-repository.ts
@@ -0,0 +1,16 @@
+import { StudentRepository } from "@/application/repositories/student-repository-interface.ts";
+import { Student } from "@/domain/entities/student.ts";
+
+export class InMemoryRepository implements StudentRepository {
+    constructor(private readonly students: Student[] = []) {}
+    
+    async create(student: Student): Promise<Record<string, number>> {
+        const newStudent = {...student, id: this.students.length + 1};
+        this.students.push(newStudent);
+        return {id: newStudent.id};
+    }
+    
+    async list(student: Partial<Student>): Promise<Student[]> {
+        return this.students;
+    }
+}
