51b68548438b1ba306c4be3d37fa27d1952af499
sergiohdljr <sergio.tjf1@gmail.com>
2025-11-09 13:01:32 -0300

    refactor: replace dropdown menu with dedicated update dialog and delete button for student actions

--- web/src/components/students/columns.tsx
+++ web/src/components/students/columns.tsx
@@ -1,8 +1,10 @@
 import type { ColumnDef } from "@tanstack/react-table"
-import { ActionTableDropdown } from "./action-table-dropdown"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { studentsApi, type Student } from "@/api/students"
 import { toast } from "sonner"
+import { UpdateStudentDialog } from "./update-student-dialog"
+import { Trash } from "lucide-react"
+import { Button } from "../ui/button"
 
 export const columns: ColumnDef<Student>[] = [
     {
@@ -53,11 +55,12 @@ export const columns: ColumnDef<Student>[] = [
           }
           
           return (
-            <ActionTableDropdown 
-              student={student}
-              onEdit={handleEdit}
-              onDelete={handleDelete}
-            />
+            <div className="flex items-end justify-end gap-2" >
+            <Button className="flex items-center justify-center" onClick={() => handleDelete(student.id)} variant="outline">
+              <Trash color="red" className="h-4 w-4" />
+            </Button>
+            <UpdateStudentDialog id={student.id} currentStudent={student} />
+            </div>
           )
         },
       },
---
+++ web/src/components/students/update-student-dialog.tsx
@@ -0,0 +1,91 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
+import type { Student, StudentPayload } from "@/api/students"
+import { studentsApi } from "@/api/students"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { toast } from "sonner"
+import { PencilIcon } from "lucide-react"
+
+  type UpdateStudentDialogProps = {
+    id: number;
+    currentStudent: Student;
+  }
+    
+
+export function UpdateStudentDialog({ id, currentStudent }: UpdateStudentDialogProps) {
+  const [student, setStudent] = useState<StudentPayload>({
+    name: currentStudent.name,
+    email: currentStudent.email,
+    cpf: currentStudent.cpf
+  })
+  const [open, setOpen] = useState(false)
+
+  const queryClient = useQueryClient()
+
+  const { mutate: updateStudent } = useMutation({
+    mutationFn: async (student: StudentPayload) => await studentsApi.update(id,student),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["students"]
+      })
+      setOpen(false)
+      toast.success("Aluno atualizado com sucesso")
+    },
+    onError: () => {
+      toast.error("Erro ao atualizar aluno")
+    }
+  })
+  const saveStudent = () => {
+    updateStudent(student)
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline"><PencilIcon className="mr-2 h-4 w-4" /></Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Atualizar Aluno</DialogTitle>
+            <DialogDescription>
+             Atualize um aluno a tabela
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Nome</Label>
+              <Input id="name-1" name="name" required value={student.name} onChange={(e) => setStudent({ ...student, name: e.target.value })}  />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="email-1">Email</Label>
+              <Input id="email-1" name="email" required value={student.email} onChange={(e) => setStudent({ ...student, email: e.target.value })}  />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="cpf-1">CPF</Label>
+              <Input id="cpf-1" name="cpf" required pattern="[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}" value={student.cpf} onChange={(e) => setStudent({ ...student, cpf: e.target.value })}  />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancelar</Button>
+            </DialogClose>
+            <Button type="submit" onClick={saveStudent}>Salvar</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  )
+}
